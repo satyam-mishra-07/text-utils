07b44709861eeb1a9c8382a6fe388ff468c79850
Satyam <satyammishrakumar39@gmail.com>
2023-11-24 14:54:43 +0530

    Router Added to react

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import Textform from "./components/Textform";
+import Textform from "./components/Textform";
 import Alert from "./components/alert";
 import About from "./components/About";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
-        msg: message,
-        Type: type
-    })
+      msg: message,
+      Type: type,
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-}
+  };
 
   const [mode, setMode] = useState("light");
   const toggleMode = () => {
-    if (mode === 'light') {
-        setMode('dark');
-        document.body.style.backgroundColor = '#042743';
-        showAlert("Dark mode has been enable", "success")
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enable", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enable", "success");
     }
-    else {
-        setMode('light');
-        document.body.style.backgroundColor = 'white';
-        showAlert("Light mode has been enable", "success")
-    }
-  }
+  };
 
   return (
-    <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        {/* <Textform
-          heading="Enter the text to analyze"
-          showAlert={showAlert}
-          mode={mode}
-          toggleMode={toggleMode}
-        /> */}
-        <About/>
-      </div>
-    </>
+    <Router>
+      <>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About mode={mode} />}></Route>
+            <Route path="/" element={<Textform heading="Enter the text to analyze" showAlert={showAlert} mode={mode} toggleMode={toggleMode}/>}></Route>
+          </Routes>
+        </div>
+      </>
+    </Router>
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
+// import PropTypes from "prop-types";
 
-export default function About() {
+
+export default function About(props) {
   return (
     <>
-    <h2>About TextUtils</h2>
-    <p>TextUtils is a Smart Online Text Editor that is made using React.Js. TextUtils provides various options to transform your text like convert the text to uppercase or lowercase, capitalise first letter of every word of the text or reverse the entered text and many more options. Keep exploring TextUtils and feel free to contribute</p>
-    <a href="https://github.com/satyam-mishra-07/text-utils.git">Contribute Here</a>
-    a
-    <h2>About Creator</h2>
-    <h3>Satyam Mishra</h3>
-    <a href="www.linkedin.com/in/satyam-mishra-53617518a">Linkedin</a>
-    <a href="https://www.instagram.com/satyam_mishra_88/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==">Instagram</a>
+    <div style={{color: props.mode==='dark'?'white':'black'}}>
+    <div className="abouttext">
+      <h2>About TextUtils</h2>
+      <p>TextUtils is a Smart Online Text Editor that is made using React.Js. TextUtils provides various options to transform your text like convert the text to uppercase or lowercase, capitalise first letter of every word of the text or reverse the entered text and many more options. Keep exploring TextUtils and feel free to contribute</p>
+      <p><a target='blank' href="https://github.com/satyam-mishra-07/text-utils.git">Contribute Here</a></p>
+    </div>
+    <div className="aboutCreator">
+      <h2>About Creator</h2>
+      <h3>Satyam Mishra</h3>
+      <p>A B-Tech First Year College Student in Jalpaiguri Government Engineering College</p>
+      <p><a target='blank' href="www.linkedin.com/in/satyam-mishra-53617518a">Linkedin</a></p>
+      <p><a target='blank' href="https://www.instagram.com/satyam_mishra_88">Instagram</a></p>
+    </div>
+    </div>
     </>
   )
 }
